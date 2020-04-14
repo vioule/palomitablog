a3e176b8e31cdc7beb9956014d8ad238df1ff4dd
Vincent <vcano.fr@outlook.com>
2020-04-14 18:18:50 +0200

    BUILD : Webpack production mode - Mise en place du bundle javascript de production avec webpack - Ajout des npm scripts pour lancer les builds :   - npm run build:frontoffice   - npm run build:backoffice - Le bundle est créer pour le browserlist par default :   - > 0.5%, last 2 versions, Firefox ESR, not dead - Le bundle est minifie sans les commentaires via TerserPlugin

---
+++ backoffice/client/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <h1>
+    Bonjour, vous êtes sur l&#39;application backoffice de Palomita Blog.
+  </h1>,
+  document.getElementById('app'),
+);
---
+++ backoffice/client/webpack.common.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'index.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
