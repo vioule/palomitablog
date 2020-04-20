c4fff64ef1a7b62aa3106562475c327cfb109798
Vincent <vcano.fr@outlook.com>
2020-04-20 10:03:37 +0200

    FIX : Frontoffice server routes - Modification du routing pour que tous les paths servent index.html

--- frontoffice/server/routes/index.js
+++ frontoffice/server/routes/index.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 module.exports = (app) => {
-  app.get('/', (req, res) => {
-    res.sendFile('index.html');
+  app.get('*', (req, res) => {
+    res.sendFile('index.html', {
+      root: `${path.resolve(__dirname, '../../')}/client/public`,
+    });
   });
 };
