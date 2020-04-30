f1e1e481263ea4c58a1a4345deecb03ad8010b32
Vincent <vcano.fr@outlook.com>
2020-04-30 08:30:38 +0200

    CI : Creation de articles dans le store - Installation de axios et redux-thunk pour async fetch data - Utilise l'API pour recuperer les articles

--- frontoffice/client/src/store/reducers.js
+++ frontoffice/client/src/store/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import menu from './menu/reducer';
+import articles from './articles/reducer';
 
 export default combineReducers({
   menu,
+  articles,
 });
