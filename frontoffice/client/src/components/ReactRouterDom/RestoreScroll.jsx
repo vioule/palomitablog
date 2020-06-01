a6231d34bc9ba320973215bfb317fcc502d4d767
Vincent <vcano.fr@outlook.com>
2020-06-01 11:41:58 +0200

    FIX : RestoreScroll - Assure l'application du scrollY a 0 lors de reload de la page - Applique un etat init qui s'active lors du montage du component

--- frontoffice/client/src/components/ReactRouterDom/RestoreScroll.jsx
+++ frontoffice/client/src/components/ReactRouterDom/RestoreScroll.jsx
@@ -1,24 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import debounce from 'lodash/debounce';
 
 export default () => {
   const history = useHistory();
+  const [init, setInit] = useState(false);
   const setScroll = () => {
     history.replace(
       history.location.pathname,
       { ...history.location.state, scrollY: window.scrollY },
     );
   };
-  const scrollY = history.location.state && 'scrollY' in history.location.state ? history.location.state.scrollY : 0;
   useEffect(() => {
     window.history.scrollRestoration = 'manual';
     window.addEventListener('scroll', debounce(setScroll, 200));
     history.replace(history.location.pathname, { ...history.location.state, scrollY: 0 });
+    setInit(true);
     return () => {
       window.removeEventListener('scroll', setScroll);
     };
   }, []);
+  const scrollY = init
+    ? history.location.state && 'scrollY' in history.location.state
+      ? history.location.state.scrollY
+      : 0
+    : 0;
   useEffect(() => {
     const restoreScroll = setTimeout(() => {
       window.scrollTo(0, scrollY);
